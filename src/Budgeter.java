57bbdae3eae336d35ec41e989e3da14246f3cb48
ionut <ionutpuiu010405@gmail.com>
2025-10-20 23:54:14 +0300

    Tested all methods!

--- src/Budgeter.java
+++ src/Budgeter.java
@@ -9,8 +9,9 @@ public class Budgeter {
         return cheap;
     }
 
-    public int getexpensive(int[] devices){
-        int expensive = devices[0];
+    public int getexpensive(int[] devices1, int[] devices2){
+        int expensive = devices1[0];
+        int[] devices = combinePrices(devices1,devices2);
         for(int i = 0; i < devices.length; i++){
             if(devices[i] > expensive){
                 expensive = devices[i];
@@ -30,18 +31,17 @@ public class Budgeter {
         return combined;
     }
 
-    public int mostexpensiveDevice(int[] devices1, int[] devices2,int budget){
-        int[] combined_price_list =  combinePrices(devices1, devices2);
+    public int mostexpensiveDevice(int[] devices1,int budget){
         int selected_price = 0;
-        for(int i = 0; i < combined_price_list.length; i++){
-            if(combined_price_list[i] > selected_price && combined_price_list[i] < budget){
-                selected_price = combined_price_list[i];
+        for(int i = 0; i < devices1.length; i++){
+            if(devices1[i] > selected_price && devices1[i] < budget){
+                selected_price = devices1[i];
             }
         }
         return selected_price;
     }
 
-    public int mostexpensiveCombo(int[] devices1, int[] devices2,int budget){
+    public int[] mostexpensiveCombo(int[] devices1, int[] devices2,int budget){
         int[] combo_keyboard_first = {0,0};
         boolean found_solution1 = false;
         int[] combo_keyboard_second = {0,0};
@@ -57,32 +57,27 @@ public class Budgeter {
                     break;
                 }
             }
-            if(!found_solution1){break;}
+            if(found_solution1){break;}
         }
-        for(int i = devices2.length-1; i >= 0; i--){
-            for(int j = devices1.length-1; j >= 0; j--){
-                if(devices2[i] + devices1[j] <= budget){
-                    combo_keyboard_first[0] = devices2[i];
-                    combo_keyboard_first[1] = devices1[j];
-                    found_solution2 = true;
-                    break;
-                }
-            }
-            if(!found_solution2){break;}
-        }
-        if(found_solution1 && !found_solution2){
-            return combo_keyboard_first[0] + combo_keyboard_first[1];
-        }
-        else if(!found_solution1 && found_solution2){
-            return combo_keyboard_second[0] + combo_keyboard_second[1];
-        }
-        else if(found_solution1 && found_solution2){
-            if(combo_keyboard_first[0] + combo_keyboard_first[1] > combo_keyboard_second[0] + combo_keyboard_second[1]){
-                return combo_keyboard_first[0] + combo_keyboard_first[1];
-            }
-            else return combo_keyboard_second[0] + combo_keyboard_second[1];
-        }
-        else return -1;
+//        for(int i = devices2.length-1; i >= 0; i--){
+//            for(int j = devices1.length-1; j >= 0; j--){
+//                if(devices2[i] + devices1[j] <= budget){
+//                    combo_keyboard_second[0] = devices1[i];
+//                    combo_keyboard_second[1] = devices2[j];
+//                    found_solution2 = true;
+//                    break;
+//                }
+//            }
+//            if(found_solution2){break;}
+//        }
+//        int first_sum = combo_keyboard_first[0] + combo_keyboard_first[1];
+//        int second_sum = combo_keyboard_second[0] + combo_keyboard_second[1];
+//        if(first_sum >= second_sum){
+//            return combo_keyboard_first;
+//        }
+//        else return combo_keyboard_second;
+        return combo_keyboard_first;
+
 
 
     }
@@ -107,7 +102,6 @@ public class Budgeter {
         int[] L = new int[n1];
         int[] R = new int[n2];
 
-        // Copy data
         for (int i = 0; i < n1; i++) L[i] = arr[left + i];
         for (int j = 0; j < n2; j++) R[j] = arr[mid + 1 + j];
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        int[] keyboard_prices = new int[] {10,9,8,7,6,5,4,3,2,1};
+        int[] USB_prices = new int[] {1,2,3,4,5,6,7,8,9,11};
+        int budget = 18;
+        Budgeter budgeter = new Budgeter();
+        int[] combo = budgeter.mostexpensiveCombo(keyboard_prices,USB_prices,budget);
+        System.out.println("The cheapest keyboard available is: " + budgeter.getcheap(keyboard_prices));
+        System.out.println("The most expensive device available is: " + budgeter.getexpensive(keyboard_prices,USB_prices));
+        System.out.println("The most expensive USB device available under budget is: " + budgeter.mostexpensiveDevice(USB_prices, budget));
+        System.out.println("The most expensive combo available under budget is: " + combo[0] + " " + combo[1]);
 
     }
 }
